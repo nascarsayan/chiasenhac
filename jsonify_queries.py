45760d5920f884ed7da105fa31dabdce37065560
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2018-11-04 03:48:23 +0530

    Stray Newline removed from query texts

--- jsonify_queries.py
+++ jsonify_queries.py
@@ -5,7 +5,7 @@ with open('queries.txt') as fp:
   queries = fp.readlines()
 with open('urls.json') as fp:
   data = json.load(fp)
-  names = [ { 'name': query } for query in queries ]
+  names = [ { 'name': query.strip() } for query in queries ]
   data.extend(names)
 with open('urls.json', 'w') as fp:
   json.dump(data, fp, sort_keys=True, indent=2)
