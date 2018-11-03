543c779af9a77870cf6740966405894dd98f3ba3
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2018-11-04 04:42:36 +0530

    Updated scrap.py

--- scrap.py
+++ scrap.py
@@ -16,20 +16,24 @@ def main():
       data = json.load(fp)
       l = len(data)
       for idx, edata in enumerate(data):
-        if 'url' not in edata or 'status' not in edata or edata['status'] == 'no':
+        if 'status' not in edata or edata['status'] == 'no':
           print('Downloading %3d of %3d : %s' % (idx, l, edata['name']))
-          response = requests.get(CSN, params={'s': edata['name']})
-          # print(response.content)
-          tree = html.fromstring((clean_html(response.content)).strip())
-          page1 = tree.xpath("//table[@class='tbtable'][1]//tr[2]//td[2]//a[@class='musictitle']/@href")[0]
-          response = requests.get(page1)
-          # print(response.content)
-          tree = html.fromstring((clean_html(response.content)).strip())
-          page2 = tree.xpath("//img[@src='http://data.chiasenhac.com/images/button_download.gif']/../@href")[0]
-          response = requests.get(page2)
-          # print(response.content)
-          tree = html.fromstring((clean_html(response.content)).strip())
-          mlink = tree.xpath("//div[@id='downloadlink2']//a[last() - 1]/@href")[0]
+          mlink = ''
+          if 'url' not in edata:
+            response = requests.get(CSN, params={'s': edata['name']})
+            # print(response.content)
+            tree = html.fromstring((clean_html(response.content)).strip())
+            page1 = tree.xpath("//table[@class='tbtable'][1]//tr[2]//td[2]//a[@class='musictitle']/@href")[0]
+            response = requests.get(page1)
+            # print(response.content)
+            tree = html.fromstring((clean_html(response.content)).strip())
+            page2 = tree.xpath("//img[@src='http://data.chiasenhac.com/images/button_download.gif']/../@href")[0]
+            response = requests.get(page2)
+            # print(response.content)
+            tree = html.fromstring((clean_html(response.content)).strip())
+            mlink = tree.xpath("//div[@id='downloadlink2']//a[last() - 1]/@href")[0]
+          else:
+            mlink = edata['url']
           os.system('aria2c "%s" -d ./downloads' % (mlink))
           data[idx]['url'] = mlink
           data[idx]['status'] = 'yes'
@@ -39,6 +43,8 @@ def main():
   except FileNotFoundError as fe:
     print('Creating urls.json... Rerun code')
   finally:
+    if len(data) > 0:
+      data[-1]['status'] = 'no'
     with open('urls.json', 'w') as fp:
       json.dump(data, fp, sort_keys=True, indent=2)
 
